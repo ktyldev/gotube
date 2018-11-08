a386b4ecf154653fce1b08e634531e6494829013
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-11-08 11:23:10 +0000

    project structure

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-contenttype="Content-Type: application/json"
-rooturl="localhost:8080"
-
-route="search"
-query=$1
-
-curl -H $contenttype -d "{\"query\":\"$query\"}" "$rooturl/$route"
