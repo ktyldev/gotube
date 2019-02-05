8bf5fa1757ce3c8ebfb4e6f0a307bd567a4b1faf
Cat Flynn <me@monodokimes.uk>
2019-02-05 03:26:04 +0000

    read version from config

--- util.go
+++ util.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 	"time"
 )
-
-var versionPath = "version.txt"
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%d\n", time.Now().UnixNano())
@@ -17,16 +13,7 @@ func Ping(w http.ResponseWriter, r *http.Request) {
 func GetVersion(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
-	s := fmt.Sprintf("gotube/%s", Version())
+	s := fmt.Sprintf("gotube/%s", Config.Version())
 
 	fmt.Fprintln(w, s)
-}
-
-func Version() string {
-	b, err := ioutil.ReadFile(versionPath)
-	if err != nil {
-		panic(err)
-	}
-
-	return strings.TrimSuffix(string(b), "\n")
 }
