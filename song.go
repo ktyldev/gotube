b408dd22bf7c67f520f0970193d5041394023cae
Cat Flynn <me@monodokimes.uk>
2019-02-08 01:00:19 +0000

    update song filename

--- song.go
+++ song.go
@@ -5,15 +5,30 @@ import (
 	"strings"
 )
 
+var (
+	_badChars = " /-" // characters in song titles to replace
+	_goodChar = "_"   // character to replace them with
+)
+
 type Song struct {
 	Title string `json:"title"` // video title
 	Id    string `json:"id"`    // youtube video id
 }
 
 func (s Song) Filename() string {
+
 	f := strings.ToLower(s.Title)
-	f = strings.Replace(f, " ", "_", -1)
-	f = fmt.Sprintf("%s.webm", f)
+	// replace bad chars
+	for _, c := range _badChars {
+		f = strings.Replace(f, string(c), _goodChar, -1)
+	}
+
+	for i := 1; i < len(f); i++ {
+		test := strings.Repeat(_goodChar, i)
+		f = strings.Replace(f, test, _goodChar, -1)
+	}
+
+	f = fmt.Sprintf("%s_%s.webm", s.Id, f)
 
 	return f
 }
