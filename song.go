6b4186e6e001a527f4754f15a66192df4999db79
Cat Flynn <me@monodokimes.uk>
2019-02-09 02:05:38 +0000

    use song id as filename

--- song.go
+++ song.go
@@ -2,12 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
-)
-
-var (
-	_badChars = " /-" // characters in song titles to replace
-	_goodChar = "_"   // character to replace them with
 )
 
 type Song struct {
@@ -16,19 +10,5 @@ type Song struct {
 }
 
 func (s Song) Filename() string {
-
-	f := strings.ToLower(s.Title)
-	// replace bad chars
-	for _, c := range _badChars {
-		f = strings.Replace(f, string(c), _goodChar, -1)
-	}
-
-	for i := 1; i < len(f); i++ {
-		test := strings.Repeat(_goodChar, i)
-		f = strings.Replace(f, test, _goodChar, -1)
-	}
-
-	f = fmt.Sprintf("%s_%s.webm", s.Id, f)
-
-	return f
+	return fmt.Sprintf("%s.webm", s.Id)
 }
